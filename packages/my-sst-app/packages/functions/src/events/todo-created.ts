6eebfe7c5b5aa494ef03354609b2717ccf8633ea
Simon Verhoeven <simon.verhoeven>
2023-09-26 00:56:00 +0800

    remove sst

--- packages/my-sst-app/packages/functions/src/events/todo-created.ts
+++
@@ -1,6 +0,0 @@
-import { EventHandler } from "sst/node/event-bus";
-import { Todo } from "@my-sst-app/core/todo";
-
-export const handler = EventHandler(Todo.Events.Created, async (evt) => {
-  console.log("Todo created", evt);
-});
